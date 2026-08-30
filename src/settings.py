29967aa18293bce2293fc478e5133533356d019e
BamNodCT <bmcintyre1113@gmail.com>
2026-08-30 19:21:27 -0400

    Updated Docker copose to use more environment variables

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,9 @@
 import os
 
 CAMERA_API = os.environ.get("CAMERA_API", "http://localhost:4444")
-BRICKTRACKER_DB = os.environ.get("BRICKTRACKER_DB", "/workspaces/legoscanner/bricktracker/app.db")
+
+BRICKTRACKER = os.environ.get("BRICKTRACKER","/workspaces/legoscanner/bricktracker")
+BRICKTRACKER_DB = os.environ.get("BRICKTRACKER_DB", f"{BRICKTRACKER}/app.db")
+IMAGE_FOLDER = os.environ.get("IMAGE_FOLDER",f"{BRICKTRACKER}/parts")
+
 BRICKOGNIZE_API = "https://api.brickognize.com"
-IMAGE_FOLDER = os.environ.get("IMAGE_FOLDER","/workspaces/legoscanner/bricktracker/parts")
